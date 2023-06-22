cd193564e091ae57cc13f477fb0fa719811ee955
Kali-Amii <amaranwachukwu232@gmail.com>
2023-06-22 10:40:58 -0700

    multiple of integer

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -14,7 +14,7 @@ int _isdigit(int c)
 	int num = c;
 	int digit = 48;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i <=9; i++)
 	{
 		if (num == digit)
 		{
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers.
+ * @a: integer one
+ * @b: integer two
+ *
+ * Return: multi
+ */
+
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	return (multi);
+}
