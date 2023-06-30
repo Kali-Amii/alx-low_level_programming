b045905a8147a0a0d5baae4e903d8dab266d8335
Kali-Amii <amaranwachukwu232@gmail.com>
2023-06-29 23:36:22 -0700

    copies string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,21 @@
-#include "main.h"
-/**
- * _strcat - joins two strings,
- * @dest: first  destination.
- * @src: source code.
- * Return: pointer to dest.
- */
-
+nclude "main.h"
+/**_strcat - concatenates two strings,@dest: destination.@src: source.Return: the pointer to dest.
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+int count = 0, count2 = 0;
 
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
+while (*(dest + count) != '\0')
+{
+count++;
+}
 
-	while (count2 >= 0)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-		{
-			break;
-		}	
-		count++;
-		count2++;
-	}
+while (count2 >= 0)
+{
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+break;
+count++;
+count2++;
+}
 	return (dest);
 
