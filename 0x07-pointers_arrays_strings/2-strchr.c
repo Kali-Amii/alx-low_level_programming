a047c77b0516c8afb769c02bb64e52d985489114
Kali-Amii <amaranwachukwu232@gmail.com>
2023-07-04 15:30:48 -0700

    Task on ALX

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: the string
+ * @c: the character housed in the string
+ * Return: returns c if character is found else NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	char food[] = { }
+	return (null); 
+}
