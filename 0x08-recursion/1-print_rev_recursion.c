1894c86a6aab287b5567f32de5e5f1360bdcb50f
Kali-Amii <amaranwachukwu232@gmail.com>
2023-07-05 19:40:49 -0700

    prints a string and also reverse the string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,7 +8,12 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		return;
-	_print_rev_recursion(s + 1);	
-	_putchar(*s);
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
