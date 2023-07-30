577456aaaa49aef1318949f2633842a470235000
Kali-Amii <amaranwachukwu232@gmail.com>
2023-07-30 15:45:09 +0100

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,14 +13,14 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0](nil)\n");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
 			printf("[%d] %s", h->str, h->len);
 		}
-	temp++;
 	h = h->next;
+	temp++;
 	}
 
 	return (temp);
