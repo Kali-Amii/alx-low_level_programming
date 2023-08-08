8e6d5e117741391b46d945ebe6f7d03c633ce599
Kali-Amii <amaranwachukwu232@gmail.com>
2023-08-07 19:03:28 -0700

    reading textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,36 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX std-output.
- * @filename: the file to open.
- * @letters: number of letters it should read and print.
- * Return: number of letters it could read and print, or 0 if:
- * - filename is NULL.
- * - the file can not be opened or read.
- * - write fails or does not write the expected amount of bytes.
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor = -1;
-	ssize_t output = 0;
-	char *buffer;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
 	if (!filename)
 		return (0);
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor < 0)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-	{
-		close(file_descriptor);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	output = read(file_descriptor, buffer, letters);
-	if (output < 0)
-	{
-		free(buffer);
-		close(file_descriptor);
-		return (0);
-	}
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	output = write(STDOUT_FILENO, buffer, output);
-	free(buffer);
-	close(file_descriptor);
-	if (output < 0)
-		return (0);
-	return (output);
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
