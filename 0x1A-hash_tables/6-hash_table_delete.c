29d9b8cfcdadcf8ee8ed2f5debc27f3a2f1466b5
Kali <amaranwachukwu232@gmail.com>
2023-10-24 10:28:25 +0100

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,17 +4,17 @@
  * hash_table_delete - Deletes a hash table.
  * @ht: A pointer to a hash table.
  */
-void hash_table_delete(hash_table_t *hat)
+void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = hat;
+	hash_table_t *head = ht;
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
-	for (i = 0; i < hat->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (hat->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			node = hat->array[i];
+			node = ht->array[i];
 			while (node != NULL)
 			{
 				tmp = node->next;
